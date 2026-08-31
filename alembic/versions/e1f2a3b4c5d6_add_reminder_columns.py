df8dd742c864d516c0412df629c8af2a67642cb9
Stepan Voytov <voytov.st.vi@gmail.com>
2026-08-31 13:51:04 +0200

    fix migration: add revision variables for alembic

--- alembic/versions/e1f2a3b4c5d6_add_reminder_columns.py
+++ alembic/versions/e1f2a3b4c5d6_add_reminder_columns.py
@@ -8,6 +8,11 @@ Create Date: 2026-08-31
 from alembic import op
 import sqlalchemy as sa
 
+revision = 'e1f2a3b4c5d6'
+down_revision = 'a1b2c3d4e5f6'
+branch_labels = None
+depends_on = None
+
 
 def upgrade() -> None:
     op.add_column("stages", sa.Column("reminded_3d", sa.Boolean(), nullable=True))
